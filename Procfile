106e4606412e9e88bdfcf1b4a98420cfc3c1a168
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-08-26 10:07:29 -0300

    Deploy da aplicação

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gerenciador_tarefas.wsgi
--- gerenciador_tarefas/settings.py
+++ gerenciador_tarefas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,5 @@ LOGIN_URL = 'logar_usuario'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
